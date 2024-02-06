1b64179a935b083690618f5df31c73fbaefca956
Ortiane <50929999+Ortiane@users.noreply.github.com>
2024-02-05 22:40:15 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-steamcmd +login anonymous +app_update 2394010 validate +quit
+sudo -u steam /usr/games/steamcmd +force_install_dir '/home/steam/Steam/steamapps/common/PalServer' +login anonymous +app_update 2394010 +quit
 sudo -u steam /home/steam/Steam/steamapps/common/PalServer/PalServer.sh -port 8211 -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
