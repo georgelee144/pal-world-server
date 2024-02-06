37d9d3c8cc815de7dee591990421209f48863779
Ortiane <50929999+Ortiane@users.noreply.github.com>
2024-02-05 21:38:49 -0500

    Added server update to script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+steamcmd +login anonymous +app_update 2394010 validate +quit
 sudo -u steam /home/steam/Steam/steamapps/common/PalServer/PalServer.sh -port 8211 -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
