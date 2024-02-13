f97173fbd0dd641f14a87d7f5cb9f037ccedd7fc
georgelee144 <43113880+georgelee144@users.noreply.github.com>
2024-02-13 18:40:54 -0500

    added discord fail message

--- safeguard.sh
+++ safeguard.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Check if Palworld is running for user "steam"
-if pgrep -u steam -x PalServer > /dev/null
-then
-    echo "Palworld is running as of $(date +%T)."
-else
-    echo "Palworld is stopped as of $(date +%T). Restarting server!"
+if ! pgrep -f "PalServer.sh" > /dev/null; then
+    /home/orchard/Documents/pal-world-server/discord.sh "OOPSIE WOOPSIE!! Uwu we made a fucky wucky!! A wittle fucko boingo! We are working VEWY HAWD to fix this!"
     /home/orchard/Documents/pal-world-server/stop.sh
     /home/orchard/Documents/pal-world-server/back_up_pal_world.sh
     /home/orchard/Documents/pal-world-server/start.sh
-    echo "Server restarted as of $(date +%T)."
+    /home/orchard/Documents/pal-world-server/discord.sh "Server has been restarted!"
 fi
